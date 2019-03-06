02d3551e10d5fb59af3a0fda52112f16b58f045a
nopast <490964186@qq.com>
2019-03-06 13:55:02 +0800

    useless function deprecation

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ class MpvueOptimizePlugin {
         // 筛选JS文件
         JSFiles = Object.keys(assets).filter(item => item.split('.').pop() === 'js')
 
-
       JSFiles.forEach(item => {
         const content = (assets[item].children instanceof Array) ? assets[item].children[0] : assets[item]
         // 修正webpack4打包时全局变量不是global的问题
