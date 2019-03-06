177b42423fa14c8b014167bb6961c821d8b74b83
nopast <490964186@qq.com>
2019-03-06 13:54:40 +0800

    useless function deprecation

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 ## 功能
 
 - 修正Webpack4提取公共模块后,全局变量不正确的问题
-- 修正Webpack4提取公共模块后,不能在入口文件引用公共模块的问题
 
 ## 安装
 
@@ -40,22 +39,7 @@ module.exports = {
     }
   },
   plugins: [
-    new MpvueOptimizePlugin({
-      entry: 'app.js',
-      chunkList: [
-        'common/manifest',
-        'common/vendor'
-      ]
-    })
+    new MpvueOptimizePlugin()
   ]
 }
 ```
-
-**配置项**
-
-|Name|Type|Required|Description|
-|:--:|:--:|:-----:|:----------|
-|**[`entry`](#)**|`{String}`| true |小程序的入口文件|
-|**[`chunkList`](#)**|`{Array<String>}`| true |打包优化的分包名称|
-
-***
--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
 class MpvueOptimizePlugin {
-  constructor(options) {
-    this.options = options
+  constructor() {
+
   }
 
   apply(compiler) {
-    const {options} = this
-    if(!options && typeof options.entry !== 'string' && !(options.chunkList instanceof Array)) return
-
     compiler.hooks.afterCompile.tap('MpvueOptimizePlugin', (compilation) => {
       const assets = compilation.assets,
         // 筛选JS文件
         JSFiles = Object.keys(assets).filter(item => item.split('.').pop() === 'js')
 
-      let requireSplitFiles = ''
-      if(options.chunkList instanceof Array) {
-        this.options.chunkList.forEach(item => {
-          // 重新引入提取出来的公共库
-          requireSplitFiles += `require('${item}');\n`
-        })
-      }
 
       JSFiles.forEach(item => {
         const content = (assets[item].children instanceof Array) ? assets[item].children[0] : assets[item]
         // 修正webpack4打包时全局变量不是global的问题
-        const nextValue = `var self = global;\n${content._value}`
-        content._value = (typeof options.entry === 'string' && item.match(this.options.entry)) ? requireSplitFiles + nextValue : nextValue
+        content._value = `var self = global || {};\n${content._value}`
       })
     })
   }
