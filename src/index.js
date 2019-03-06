835a4ed81e8baa65a2f63c1b9c02b56ef8c16a12
nopast <490964186@qq.com>
2019-03-06 16:30:37 +0800

    enhance compatibility

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ MpvueOptimizePlugin.prototype.apply = function(compiler) {
   compiler.hooks.afterCompile.tap('MpvueOptimizePlugin', (compilation) => {
     const assets = compilation.assets,
       // 筛选JS文件
-      JSFiles = Object.keys(assets).filter(item => item.split('.').pop() === 'js')
+      JSFiles = Object.keys(assets).filter(item => /\.js$/.test(item))
 
     JSFiles.forEach(item => {
       const content = (assets[item].children instanceof Array) ? assets[item].children[0] : assets[item]
       // 修正webpack4打包时全局变量不是global的问题
-      content._value = `var self = global || {};\n${content._value}`
+      content._value = `var self = self || global || {};\n${content._value}`
     })
   })
 }
