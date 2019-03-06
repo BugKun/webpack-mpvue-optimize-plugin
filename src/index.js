80054fd77fd9115c4fd9c4d506a4dcabe2cc7fea
nopast <490964186@qq.com>
2019-03-06 13:58:27 +0800

    useless function deprecation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-class MpvueOptimizePlugin {
-  constructor() {
+function MpvueOptimizePlugin() {}
 
-  }
+MpvueOptimizePlugin.prototype.apply = function(compiler) {
+  compiler.hooks.afterCompile.tap('MpvueOptimizePlugin', (compilation) => {
+    const assets = compilation.assets,
+      // 筛选JS文件
+      JSFiles = Object.keys(assets).filter(item => item.split('.').pop() === 'js')
 
-  apply(compiler) {
-    compiler.hooks.afterCompile.tap('MpvueOptimizePlugin', (compilation) => {
-      const assets = compilation.assets,
-        // 筛选JS文件
-        JSFiles = Object.keys(assets).filter(item => item.split('.').pop() === 'js')
-
-      JSFiles.forEach(item => {
-        const content = (assets[item].children instanceof Array) ? assets[item].children[0] : assets[item]
-        // 修正webpack4打包时全局变量不是global的问题
-        content._value = `var self = global || {};\n${content._value}`
-      })
+    JSFiles.forEach(item => {
+      const content = (assets[item].children instanceof Array) ? assets[item].children[0] : assets[item]
+      // 修正webpack4打包时全局变量不是global的问题
+      content._value = `var self = global || {};\n${content._value}`
     })
-  }
+  })
 }
 
 module.exports = MpvueOptimizePlugin
